5b38fc16eb3a3f760394e7098a5c4474713a451f
Carlos Escate <stornblood6969@gmail.com>
2025-11-13 07:42:26 -0500

    Add filament admin theme css on vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/validation-documents.css'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/validation-documents.css',
+                'resources/css/filament/admin/theme.css',
+            ],
             refresh: true,
         }),
         tailwindcss(),
