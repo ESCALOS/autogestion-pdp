dd9a73c33493db598e124246404360fbed395186
Jesus Pachas Saravia <j.pachas_ext@tpparacas.com.pe>
2025-11-17 07:18:23 -0500

    Add new view for appeals company and drivers

---
+++ resources/views/components/layouts/appeal.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('partials.head', ['title' => $title ?? config('app.name')])
+    </head>
+    <body class="bg-white antialiased dark:bg-gray-900">
+        <header class="bg-primary-500 w-full py-4 text-white">
+            <div class="mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+                <h1 class="text-xl font-bold">{{ config('app.name') }}</h1>
+                <img src="{{ asset('images/logo-pdp.webp') }}" alt="{{ config('app.name') }} Logo" class="h-12" />
+            </div>
+        </header>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            {{ $slot }}
+        </div>
+        @livewire('notifications')
+        @filamentScripts
+    </body>
+</html>
